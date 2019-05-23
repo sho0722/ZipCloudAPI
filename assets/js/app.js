3dd85f09b674258d4b42fde857180092fba14f0b
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-05-23 15:12:22 +0900

    jsファイルに追加

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    //検索ボタンをくリックした時の処理
+    $("#search-btn").on("click", function() {
+
+        //検索ワードを取得する
+        let searchWord = $("#search-word").val();
+
+        //郵便番号の検索をする
+        
+    })
+})
