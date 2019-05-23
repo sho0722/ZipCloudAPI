9e812bf7e1e61a9a4f76c6b9ac649b45f1642f20
Sho Tanaka <swallowtail.722.kyoto@gmail.com>
2019-05-24 00:44:15 +0900

    未入力の場合の処理の追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,34 +6,40 @@ $(function() {
         //検索ワードを取得する
         let searchWord = $("#search-word").val();
 
-        //郵便番号の検索をしに行く
-        $.ajax({
-            //データの通信をするところ
-            url: "http://zipcloud.ibsnet.co.jp/api/search",
-            type: "GET",
-            dataType: "jsonp",
-            data: {
-                zipcode: searchWord
-            }
+        if (searchWord == "") {
+            alert("未入力です");
+            return;
+        
+        } else {
 
-        }).done( (data) => {
-            //通信が成功した時
+            //郵便番号の検索をしに行く
+            $.ajax({
+                //データの通信をするところ
+                url: "http://zipcloud.ibsnet.co.jp/api/search",
+                type: "GET",
+                dataType: "jsonp",
+                data: {
+                    zipcode: searchWord
+                }
 
-            for (item of data.results) {
+            }).done( (data) => {
+                //通信が成功した時
 
-                let prefecture = item.address1;
-                let city = item.address2;
-                let address = item.address3;
+                let prefecture = data.results[0].address1;
+                let city = data.results[0].address2;
+                let address = data.results[0].address3;
 
                 $("#prefecture").text(prefecture);
                 $("#city").text(city);
                 $("#address").text(address);
-            }
-        
+            
 
-        }).fail( (error) => {
-            // 通信が失敗した時
-            console.log(error);
-        })
+            }).fail( (error) => {
+                // 通信が失敗した時
+                console.log(error);
+            })
+            
+        }
+
     })
 })
